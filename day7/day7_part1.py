67d1e14d161b815c8ce05e53c04aff54d1167760
Alaro Abdulroqeeb <abdulroqeebalaro5@gmail.com>
2023-05-25 12:28:53 +0100

    updated into folder

--- day7/day7_part1.py
+++ day7/day7_part1.py
@@ -4,17 +4,8 @@ def parse_directory_listing(listing):
     filesystem = {}
     current_dir = filesystem
     stack = [current_dir]
-    new = []
 
-    for dir in listing:
-        dir = dir.replace("cd ", "dir ")
-    #     # for line in dir:
-    #         # new.append(line)
-    #     # listy.append(dir)
-
-    #     print(dir)
-
-    for line in dir:
+    for line in listing:
         if line.startswith("dir "):
             directory_name = line[4:]
             current_dir[directory_name] = {}
@@ -23,23 +14,16 @@ def parse_directory_listing(listing):
         elif re.match(r"\d+ \w+", line):
             file_size, file_name = line.split()
             current_dir[file_name] = int(file_size)
-            # print(file_name)
-        elif line == "dir ..":
+        # elif line.startswith("$ cd "):
+        #     directory_name = line[4:]
+        #     current_dir[directory_name] = {}
+        #     current_dir = current_dir[directory_name]
+        #     stack.append(current_dir)
+        #     # print(value)
+        elif line == "$ cd ..":
             stack.pop()
             current_dir = stack[-1]
-        # elif line.startswith("cd "):
-        #     line.replace("cd ", "dir ")
-        #     directory_name = line[2:]
-        #     current_dir = current_dir
-        #     stack.append(current_dir)
-        # elif line.startswith("cd "):
-        #     directory = line.split()[1]
-        #     if directory == "..":
-        #         current_directory = "/".join(current_directory.split("/")[:-2]) + "/"
-        #     else:
-        #         current_directory = "/"
-        #         current_directory = current_directory + directory + "/"
-
+    # print(value)
     return filesystem
 
 def calculate_total_size(directory):
@@ -58,23 +42,19 @@ def find_directories_within_limit(filesystem, limit):
     while stack:
         directory, path = stack.pop()
         total_size = calculate_total_size(directory)
-        # print(directory)
 
-        if total_size <= limit and path != "":
+        if total_size <= limit:
             directories_within_limit.append(total_size)
-            # print(total_size)
+
         for name, item in directory.items():
             if isinstance(item, dict):
                 stack.append((item, f"{path}/{name}"))
+        # print(stack)
 
     return directories_within_limit
 
-with open("test7.txt", "r") as file:
+with open("day7_data.txt", "r") as file:
     listing = file.read().split("\n")
-    for dir in listing:
-        dir = dir.replace("cd ", "dir ")
-        # print(dir)
-
 filesystem = parse_directory_listing(listing)
 directories_within_limit = find_directories_within_limit(filesystem, 100000)
 sum_of_total_sizes = sum(directories_within_limit)
